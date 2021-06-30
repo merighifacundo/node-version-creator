b511ac437c59bd74f2b2541b1165050d26dd999c
Facundo Merighi <merighifacundo@gmail.com>
2021-06-30 08:51:02 -0300

    adding some logging

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const githubService  = require("./lib/github");
 try {
-    let newVersion = core.getInput('new-version');
-    let oldVersion = core.getInput('old-version');
-    let additionalFiles = core.getInput('additional-files');
+    const newVersion = core.getInput('new-version');
+    const oldVersion = core.getInput('old-version');
+    const additionalFiles = core.getInput('additional-files');
+    const createReleaseBranch = core.getInput('release-branch') !== 'false';
     const client = github.getOctokit(core.getInput('token'))
-    githubService.createTag(newVersion, client, github, additionalFiles, oldVersion).catch((error) => {
+    console.log(`Welcome to Node Version Creator this workflow will create a new tag and could create a release branch with the new version.`)
+    console.log(`The new version number: ${newVersion}, The old version number: ${oldVersion}`)
+    console.log(`Additional files for the versioning commit: ${additionalFiles}`)
+    console.log(`Should create the releaseBranch? ${createReleaseBranch}`)
+    githubService.createTag(newVersion, client, github, additionalFiles, oldVersion, createReleaseBranch).catch((error) => {
         core.setFailed(error.message);
     })
 } catch (error) {
